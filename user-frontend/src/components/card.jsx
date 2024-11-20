bbde15b5ed52972ac98dc736cb89f8e02c63a6f9
EmilDalhoff <112545316+EmilDalhoff@users.noreply.github.com>
2024-11-20 22:09:33 +0100

    changed a lot really, completed a simple card with placeholder text. made the footer with the transofrm animation we want and also a modal afterwards. I also added fonts which we need for further use and a whole lot more, gg.

--- user-frontend/src/components/card.jsx
+++ user-frontend/src/components/card.jsx
@@ -9,61 +9,62 @@ export default function ProductList() {
       size: "80 L",
       color: "Sort",
       status: "Aktivt opslag",
-      image: "../public/dummypicture.webp", // Replace with your placeholder image
+      image: "../dummypicture.webp",
     },
   ];
 
   return (
-    <div>
-      <div className="space-y-4 min-w-[300px]">
-        {products.map((product) => (
-          <div
-            key={product.id}
-            className="relative bg-white rounded-lg shadow-lg p-6 flex items-center overflow-hidden "
+    <div className="w-full p-2 flex flex-col space-y-4 mx-auto">
+      {products.map((product) => (
+        <div
+          key={product.id}
+          className="relative bg-white rounded-lg shadow-lg flex flex-col items-start overflow-hidden"
+        >
+          <svg
+            className="absolute right-0 top-0 h-full w-auto object-cover pointer-events-none"
+            width="103"
+            height="144"
+            viewBox="0 0 103 144"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
           >
-            <svg
-              className="absolute right-0 top-0 h-full w-auto object-cover pointer-events-none"
-              width="103"
-              height="144"
-              viewBox="0 0 103 144"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M79.4404 105.271C79.4404 103.566 79.0981 102.458 78.4135 101.947C77.9001 101.265 77.3012 100.412 76.6165 99.3891C70.6267 102.97 62.3265 104.76 51.716 104.76C37.5116 104.76 25.7033 100.071 16.2907 90.6939C6.02242 80.9756 0.888306 68.5294 0.888306 53.3554C0.888306 34.4304 6.79255 19.6825 18.601 9.1118C30.5806 -1.62939 45.8974 -7 64.5514 -7C86.2858 -7 103.399 0.416556 115.893 15.2496C128.557 30.0827 134.888 48.8373 134.888 71.5132C134.888 99.6448 127.273 121.468 112.042 136.983C96.9819 152.328 78.9269 160 57.877 160C44.6995 160 34.089 157.102 26.0455 151.306C18.0021 145.337 13.9803 137.154 13.9803 126.754C13.9803 118.74 16.034 112.347 20.1413 107.573C29.5537 113.37 39.1375 116.268 48.8923 116.268C58.8183 116.268 66.3483 115.33 71.4825 113.455C76.7876 111.58 79.4404 108.852 79.4404 105.271Z"
-                fill="#FF7127"
-              />
-            </svg>
+            <path
+              d="M79.4404 105.271C79.4404 103.566 79.0981 102.458 78.4135 101.947C77.9001 101.265 77.3012 100.412 76.6165 99.3891C70.6267 102.97 62.3265 104.76 51.716 104.76C37.5116 104.76 25.7033 100.071 16.2907 90.6939C6.02242 80.9756 0.888306 68.5294 0.888306 53.3554C0.888306 34.4304 6.79255 19.6825 18.601 9.1118C30.5806 -1.62939 45.8974 -7 64.5514 -7C86.2858 -7 103.399 0.416556 115.893 15.2496C128.557 30.0827 134.888 48.8373 134.888 71.5132C134.888 99.6448 127.273 121.468 112.042 136.983C96.9819 152.328 78.9269 160 57.877 160C44.6995 160 34.089 157.102 26.0455 151.306C18.0021 145.337 13.9803 137.154 13.9803 126.754C13.9803 118.74 16.034 112.347 20.1413 107.573C29.5537 113.37 39.1375 116.268 48.8923 116.268C58.8183 116.268 66.3483 115.33 71.4825 113.455C76.7876 111.58 79.4404 108.852 79.4404 105.271Z"
+              fill="#FF7127"
+            />
+          </svg>
 
-            <div className="flex-1 relative">
-              <h3 className="text-xl text-gray-600 font-bold">
-                {product.title}
-              </h3>
-              <p className="text-gray-600">
-                <span className="font-semibold">Tilstand:</span>{" "}
+          <div className="pl-5 p-1 grid gap-4 relative z-10">
+            <h3 className="text-xl pt-2 text-[#060606] font-bold">
+              {product.title}
+            </h3>
+            <div className="grid gap-1">
+              <p className="text-[#888D96] text-xs">
+                <span className="font-medium">Tilstand:</span>{" "}
                 {product.condition}
               </p>
-              <p className="text-gray-600">
-                <span className="font-semibold">Størrelse:</span> {product.size}
+              <p className="text-[#888D96] text-xs">
+                <span className="font-medium">Størrelse:</span> {product.size}
               </p>
-              <p className="flex items-center text-gray-600">
-                <span className="w-4 h-4 bg-black rounded-full inline-block mr-2"></span>
+              <p className="flex items-center text-gray-600 text-xs">
+                <span className="w-3 h-3 bg-black rounded-full inline-block mr-2"></span>
                 {product.color}
               </p>
-              <p className="text-green-500 font-semibold">{product.status}</p>
             </div>
-
-            {/* Image */}
-            <div className="relative z-10 w-20 h-20 flex-shrink-0">
-              <img
-                src={product.image}
-                alt={product.title}
-                className="object-contain w-full h-full"
-              />
-            </div>
+            <p className="text-[#5BAD86] font-semibold text-xs ">
+              {product.status}
+            </p>
           </div>
-        ))}
-      </div>
+
+          <div className="absolute right-4 top-4 z-10 w-16 h-16 flex-shrink-0">
+            <img
+              src={product.image}
+              alt={product.title}
+              className="object-contain rounded-xl"
+            />
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
--- user-frontend/src/components/navigation.jsx
+++ user-frontend/src/components/navigation.jsx
@@ -1,31 +1,86 @@
-import react from "react";
-import { FaHouse, FaBookmark, FaUserLarge, FaPlus } from "react-icons/fa6";
+"use client";
+import React, { useState } from "react";
+import { FaHouse, FaBookmark, FaUserLarge, FaPlus } from "react-icons/fa6"; // FaTimes for 'x'
 import { PiHandbagSimpleFill } from "react-icons/pi";
+import { FaTimes } from "react-icons/fa";
 
 export default function Navigation() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
   return (
-    <footer className="fixed bottom-0 left-0 w-full bg-white shadow-md flex items-center justify-between px-4 py-1">
-      <button className="flex flex-col items-center text-gray-400">
-        <FaHouse size={30} />
-      </button>
+    <>
+      <footer className="fixed bottom-0 left-0 w-full bg-white shadow-md flex items-center justify-between px-4 py-1">
+        <button className="flex flex-col items-center text-gray-400">
+          <FaHouse size={30} />
+        </button>
 
-      <button className="flex flex-col items-center text-gray-400">
-        <FaBookmark size={25} />
-      </button>
+        <button className="flex flex-col items-center text-gray-400">
+          <FaBookmark size={25} />
+        </button>
 
-      <button className="w-20 h-20 bg-[#53BF6D] text-white rounded-full flex items-center justify-center shadow-lg -translate-y-9">
-        <span className="text-2xl">
-          <FaPlus size={24} />
-        </span>
-      </button>
+        <button
+          onClick={toggleModal}
+          className={`w-20 h-20  rounded-full flex items-center justify-center shadow-lg -translate-y-9 transition-all duration-300 ${
+            isModalOpen ? "bg-[#FF7127]" : "bg-[#53BF6D]"
+          }`}
+        >
+          <span
+            className={`text-2xl text-white transform transition-transform duration-300 ${
+              isModalOpen ? "rotate-45" : "rotate-0"
+            }`}
+          >
+            {isModalOpen ? <FaTimes size={24} /> : <FaPlus size={24} />}
+          </span>
+        </button>
 
-      <button className="flex flex-col items-center text-gray-400">
-        <PiHandbagSimpleFill size={30} />
-      </button>
+        <button className="flex flex-col items-center text-gray-400">
+          <PiHandbagSimpleFill size={30} />
+        </button>
 
-      <button className="flex flex-col items-center text-gray-400">
-        <FaUserLarge size={24} />
-      </button>
-    </footer>
+        <button className="flex flex-col items-center text-gray-400">
+          <FaUserLarge size={24} />
+        </button>
+      </footer>
+
+      <div
+        className={`fixed inset-0 bg-black bg-opacity-50 flex justify-center items-end transition-opacity duration-300 ${
+          isModalOpen
+            ? "opacity-100 pointer-events-auto"
+            : "opacity-0 pointer-events-none"
+        }`}
+        onClick={toggleModal} // Close the modal when clicking outside
+      >
+        <div
+          className={`bg-white w-full max-w-lg h-[700px] rounded-t-lg p-6 shadow-lg transform transition-transform duration-300 ${
+            isModalOpen ? "translate-y-0" : "translate-y-full"
+          }`}
+          onClick={(e) => e.stopPropagation()} // Prevent click from closing modal
+        >
+          <div className="flex justify-center items-center flex-col">
+            <h2
+              className="text-5xl text-[#53BF6D] mb-4"
+              style={{ fontFamily: "Modak" }}
+            >
+              LEND'<span className="text-[#FF7127]">r</span>
+            </h2>
+            <p className="text-xl text-center text-[#060606] mb-6">
+              Opret opslag eller produkt
+            </p>
+            <div className="flex space-x-4 font-bold">
+              <button className="px-4 py-2 bg-[#2B8F60] text-white text-semibold rounded-lg shadow-md">
+                Opret produkt
+              </button>
+              <button className="px-4 py-2 bg-[#5BAD86] text-white rounded-lg shadow-md">
+                Opret ordre
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
