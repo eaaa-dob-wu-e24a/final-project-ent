9d116d14872d2c02d6718b115aced0e399a8fa60
TobiasRW <tobiasrw98@gmail.com>
2024-11-21 14:47:36 +0100

    order card

--- user-frontend/src/app/page.jsx
+++ user-frontend/src/app/page.jsx
@@ -5,15 +5,14 @@ import ProductForm from "@/components/product-form";
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen bg-[#F7F9FD]">
-      {/* Main content */}
-      <main className="flex-grow flex flex-col gap-8 pt-10 items-center sm:items-start">
+    <>
+      
         <ProductList />
         <ProductForm />
         <Navigation />
 
         <LoginForm />
-      </main>
-    </div>
+      
+    </>
   );
 }
