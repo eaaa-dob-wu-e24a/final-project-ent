7019405ae188b060a786d957e2f367b56b239bd8
TobiasRW <tobiasrw98@gmail.com>
2024-11-22 11:38:28 +0100

    .. remove import from '/'

--- user-frontend/src/app/page.jsx
+++ user-frontend/src/app/page.jsx
@@ -1,11 +1,7 @@
 import LoginForm from "../components/login-form";
-import TopUi from "../components/top-section";
 export default function Home() {
   return (
     <>
-      <TopUi />
-      <ProductList />
-      <Navigation />
       <LoginForm />
     </>
   );
