ae14252867c0cb92367b5f4c4fc3f82e384ecd17
Nicolai Harms <nicolai.harms@gmail.com>
2024-11-22 12:04:38 +0100

    Updated styling to make sure all is aligned.

--- user-frontend/src/app/page.jsx
+++ user-frontend/src/app/page.jsx
@@ -1,11 +1,8 @@
 import LoginForm from "../components/login-form";
-
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen bg-[#F7F9FD]">
-      <main className="flex-grow flex flex-col gap-8 pt-10 items-center sm:items-start">
-        <LoginForm />
-      </main>
-    </div>
+    <>
+      <LoginForm />
+    </>
   );
 }
