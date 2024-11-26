bf7904e7d18e052b7930417809ecddcb0fbad6a2
EmilDalhoff <112545316+EmilDalhoff@users.noreply.github.com>
2024-11-26 08:59:27 +0100

    setup remix and ShadCN

---
+++ support-frontend/tailwind.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from "tailwindcss";
+
+export default {
+    darkMode: ["class"],
+    content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+  	extend: {
+  		fontFamily: {
+  			sans: [\n          "Inter",\n          "ui-sans-serif",\n          "system-ui",\n          "sans-serif",\n          "Apple Color Emoji",\n          "Segoe UI Emoji",\n          "Segoe UI Symbol",\n          "Noto Color Emoji",\n        ]
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		colors: {}
+  	}
+  },
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config;
