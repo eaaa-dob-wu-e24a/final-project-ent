dc812233d449052ea783447f01c6330d79363951
Nicolai Harms <Nicolai.harms@gmail.com>
2024-12-04 11:06:17 +0100

    Setup for remix.

--- support-frontend/tailwind.config.ts
+++ support-frontend/tailwind.config.ts
@@ -1,20 +1,22 @@
 import type { Config } from "tailwindcss";
 
 export default {
-    darkMode: ["class"],
-    content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
+  content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
   theme: {
-  	extend: {
-  		fontFamily: {
-  			sans: [\n          "Inter",\n          "ui-sans-serif",\n          "system-ui",\n          "sans-serif",\n          "Apple Color Emoji",\n          "Segoe UI Emoji",\n          "Segoe UI Symbol",\n          "Noto Color Emoji",\n        ]
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {}
-  	}
+    extend: {
+      fontFamily: {
+        sans: [
+          "Inter",
+          "ui-sans-serif",
+          "system-ui",
+          "sans-serif",
+          "Apple Color Emoji",
+          "Segoe UI Emoji",
+          "Segoe UI Symbol",
+          "Noto Color Emoji",
+        ],
+      },
+    },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [],
 } satisfies Config;
