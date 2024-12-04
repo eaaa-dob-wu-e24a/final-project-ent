0dccaf6e8a67a8575b8e9a0c9a362a7e5f90d3af
TobiasRW <tobiasrw98@gmail.com>
2024-12-04 10:46:51 +0100

    update product frontend + backend

--- user-frontend/src/app/ordre-og-opslag/produkt/[product_id]/page.jsx
+++ user-frontend/src/app/ordre-og-opslag/produkt/[product_id]/page.jsx
@@ -1,5 +1,9 @@
-import { getSpecificProduct } from "@/actions/products.actions";
+import { getSpecificProduct, updateProduct } from "@/actions/products.actions";
 import Image from "next/image";
+import Link from "next/link";
+import UpdateProduct from "../../components/update-product";
+import { IoIosArrowBack } from "react-icons/io";
+
 
 export default async function Page({ params }) {
   // Get product_id from params
@@ -9,19 +13,32 @@ export default async function Page({ params }) {
   const product = await getSpecificProduct(product_id);
 
   return (
-    <div className="p-4">
-      <h1 className="text-text font-bold text-4xl">{product.name}</h1>
-      <p>Tilstand: {product.product_condition}</p>
-      <p>Størrelse: {product.size}</p>
-      <p>Brand: {product.brand}</p>
-      <p>Farve: {product.color}</p>
-      <p>Produkt type: {product.product_type}</p>
-      <Image
-        src={`${process.env.NEXT_PUBLIC_API_URL}/api/product/create/${product.picture_path}`}
-        alt={product.name}
-        width={100}
-        height={100}
-      />
+    <div className="flex flex-col gap-6 bg-whitebg">
+      <section className="bg-[#EFF1F5] pb-8 rounded-b-3xl">
+        <div className="grid grid-cols-3 items-center w-11/12 mx-auto py-6">
+          <Link href="/ordre-og-opslag" className="text-sm flex items-center gap-1">
+          <IoIosArrowBack /> <p>Tilbage</p>
+          </Link>
+          <h1 className="text-text text-xl font-bold text-center text-nowrap justify-self-center">
+            {product.name}
+          </h1>
+        </div>
+        <div className="w-10/12 mx-auto">
+          <div className="w-full shadow-md rounded-lg">
+            <Image
+              className="rounded-lg w-full h-60 object-cover"
+              src={`${process.env.NEXT_PUBLIC_API_URL}/api/product/create/${product.picture_path}`}
+              alt={product.name}
+              width={100}
+              height={100}
+            />
+          </div>
+        </div>
+      </section>
+      <section className="w-10/12 mx-auto flex flex-col gap-4">
+        <UpdateProduct product={product} updateProduct={updateProduct}/>
+      </section>
     </div>
   );
 }
+
