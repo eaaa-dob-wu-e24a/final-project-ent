b0c4c2e14a6ae4f98422268f60620d506d6ef81c
TobiasRW <tobiasrw98@gmail.com>
2024-12-11 15:58:36 +0100

    next img config fix

--- user-frontend/next.config.mjs
+++ user-frontend/next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["localhost"],
+    domains: [
+      "localhost", // for local development
+      "lendr.tobiaswolmar.dk", // your production domain
+    ],
   },
 };
 
